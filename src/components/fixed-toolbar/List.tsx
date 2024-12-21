641e33f7d95b2ae364d512d248dce413d475f447
qinluhe <qinluhe.twodog@gmail.com>
2024-12-21 15:26:17 +0800

    fix: support toggle type

--- src/components/fixed-toolbar/List.tsx
+++ src/components/fixed-toolbar/List.tsx
@@ -1,5 +1,5 @@
 import { useFocused, useReadOnly, useSlate } from 'slate-react';
-import { isBlockActive, turnToType } from '@/lib/editor';
+import { getBlock, isBlockActive, turnToType } from '@/lib/editor';
 import { useCallback } from 'react';
 import { NodeType } from '@/types';
 import { Button, ButtonProps } from '@/components/ui/button';
@@ -12,13 +12,21 @@ function List() {
   const readOnly = useReadOnly();
   const focused = useFocused() && document.getSelection()?.type !== 'Node';
   const editor = useSlate();
+  const entry = getBlock(editor);
+  const activeListType = entry ? entry[0].type : NodeType.Paragraph;
+
   const handleClick = useCallback((type: NodeType) => {
     return (e: React.MouseEvent) => {
       e.preventDefault();
       e.stopPropagation();
-      turnToType(editor, type);
+      if (activeListType === type) {
+        turnToType(editor, NodeType.Paragraph);
+      } else {
+        turnToType(editor, type);
+      }
+
     };
-  }, [editor]);
+  }, [activeListType, editor]);
 
   const getButtonProps = useCallback((type: NodeType) => {
     const isActive = isBlockActive(editor, type);
